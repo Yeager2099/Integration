ac4216c0d15b18cc12ae38db98034cd6e70dce70
Yeager2099 <104197243+Yeager2099@users.noreply.github.com>
2025-07-27 19:29:59 +0800

    Update bridge.py

--- bridge.py
+++ bridge.py
@@ -1,19 +1,19 @@
 from web3 import Web3
 from web3.providers.rpc import HTTPProvider
-from web3.middleware import ExtraDataToPOAMiddleware #Necessary for POA chains
+from web3.middleware import ExtraDataToPOAMiddleware  # Necessary for POA chains
 from datetime import datetime
 import json
 import pandas as pd
 
 
 def connect_to(chain):
-    if chain == 'source':  # The source contract chain is avax
-        api_url = f"https://api.avax-test.network/ext/bc/C/rpc" #AVAX C-chain testnet
+    if chain == 'source':  # The source contract chain is AVAX
+        api_url = f"https://api.avax-test.network/ext/bc/C/rpc"  # AVAX C-chain testnet
 
-    if chain == 'destination':  # The destination contract chain is bsc
-        api_url = f"https://data-seed-prebsc-1-s1.binance.org:8545/" #BSC testnet
+    if chain == 'destination':  # The destination contract chain is BSC
+        api_url = f"https://data-seed-prebsc-1-s1.binance.org:8545/"  # BSC testnet
 
-    if chain in ['source','destination']:
+    if chain in ['source', 'destination']:
         w3 = Web3(Web3.HTTPProvider(api_url))
         # inject the poa compatibility middleware to the innermost layer
         w3.middleware_onion.inject(ExtraDataToPOAMiddleware, layer=0)
@@ -26,7 +26,7 @@ def get_contract_info(chain, contract_info):
         This function is used by the autograder and will likely be useful to you
     """
     try:
-        with open(contract_info, 'r')  as f:
+        with open(contract_info, 'r') as f:
             contracts = json.load(f)
     except Exception as e:
         print(f"Failed to read contract info\nPlease contact your instructor\n{e}")
@@ -63,6 +63,9 @@ def scan_blocks(chain, contract_info="contract_info.json"):
         source_contract_abi = contracts['source_contract_abi']
         destination_contract_address = contracts['destination_contract_address']
         destination_contract_abi = contracts['destination_contract_abi']
+
+    # Debug: Print the source contract address to ensure it's correct
+    print(f"Source contract address: {source_contract_address}")
 
     # Instantiate the contracts
     source_contract = w3.eth.contract(address=source_contract_address, abi=source_contract_abi)
